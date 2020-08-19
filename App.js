be0b8237627b75ed16f78ac801167113c5325386
rojerthat07 <jercarlopogi07@gmail.com>
2020-08-19 11:24:06 +0800

    header style

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react'
 import Icon from 'react-native-vector-icons'
-
+import {StyleSheet} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './components/Home'
@@ -15,7 +15,9 @@ class App extends Component {
     return (
       <NavigationContainer>
        <Stack.Navigator initialRouteName="Home" > 
-       <Stack.Screen name="Home"  options={{ title: 'Overview' }} component={Home} />
+       <Stack.Screen  name="Home"  options={{ title: 'Corana Virus Updates',headerStyle:{
+         backgroundColor:'black'
+       },headerTintColor:'white' }} component={Home} />
         <Stack.Screen name="Details" component={About} />
       </Stack.Navigator>
       </NavigationContainer>
@@ -24,5 +26,10 @@ class App extends Component {
 }
 
 
+const styles = StyleSheet.create({
+  header:{
+    backgroundColor:'black'
+  }
+})
 
 export default App
